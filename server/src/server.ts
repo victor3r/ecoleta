c33302eccd684042998f89b44bb8546fe73acb64
victor <victorabelo1697@gmail.com>
2020-06-02 17:58:29 -0300

    Criando a base da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+import routes from './routes';
 
+dotenv.config();
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel',
-  ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
